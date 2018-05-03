81365cc1663d2741bd8962aeaf265c25e830aac9
Guillermo Saavedra <guillermosaavedra@guillermos-mbp.nvstu.edu>
2018-05-02 22:32:34 -0400

    .gitignore file added and update on program

--- Postfix.java
+++ Postfix.java
@@ -1,49 +1,57 @@
 import java.util.*;
-public class Postfix {
 
-  public static void main(String[] args) {
-    Stack<String> stack = new Stack<>();
-    double num1;
-    double num2;
-    double result;
+class Postfix {
+  public static void main(String args[]){
+    HashMap <Character, Double> valueMap = new HashMap<>();
+    Stack <Double> stack = new Stack<>();
+    Scanner scanner = new Scanner(System.in);
+
     for (int i=0; i < args.length; i++) {
-      String value = args[i];
-      try {
-        int intValue = Integer.parseInt(args[i]);
-        stack.push(value);
-        System.out.println("push "+intValue);
-      } catch (Exception e) {
-        switch(value) {
-          case "-": 
-            System.out.println("subt");
-            num1 = Double.parseDouble(stack.pop());
-            num2 = Double.parseDouble(stack.pop());
+      char input = args[i].charAt(0);
+      if (Character.isLetter(input) && (input!='x')) {
+        if (!valueMap.containsKey(input)) {
+          System.out.println("Enter the value for "+input);
+          double value = scanner.nextDouble();
+          stack.push(value);
+          valueMap.put(input,value);
+        } else {
+          stack.push(valueMap.get(input));
+        }
+      } else {
+        double num1;
+        double num2;
+        double result;
+        switch(input) {
+          case '-': 
+            System.out.println("SUBT");
+            num1 = stack.pop();
+            num2 = stack.pop();
             result = num2 - num1;
-            stack.push(result+"");
+            stack.push(result);
           break;
-          case "+": System.out.println("add");
-            num1 = Double.parseDouble(stack.pop());
-            num2 = Double.parseDouble(stack.pop());
+          case '+': System.out.println("ADD");
+            num1 = stack.pop();
+            num2 = stack.pop();
             result = num1+num2;
-            stack.push(result+"");
+            stack.push(result);
           break;
-          case "x": System.out.println("mult");
-            num1 = Double.parseDouble(stack.pop());
-            num2 = Double.parseDouble(stack.pop());
+          case 'x': System.out.println("MULT");
+            num1 = stack.pop();
+            num2 = stack.pop();
             result = num1 * num2;
-            stack.push(result+"");
+            stack.push(result);
           break;
-          case "/": System.out.println("div");
-            num1 = Double.parseDouble(stack.pop());
-            num2 = Double.parseDouble(stack.pop());
+          case '/': System.out.println("DIV");
+            num1 = stack.pop();
+            num2 = stack.pop();
             result = num2/num1;
-            stack.push(result+"");
+            stack.push(result);
           break;
           default: System.out.println("Invalid input");
         }
       }
     }
-    double finalResult = Double.parseDouble(stack.pop());
+    double finalResult = stack.pop();
     System.out.println("Final result : "+finalResult);
   }
 }
